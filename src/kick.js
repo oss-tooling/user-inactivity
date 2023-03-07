9ff3e00a04595fe2cc990c792c4fe26e3514e239
Brett Logan <lindluni@github.com>
2023-03-07 11:20:02 -0500

    Run standard linter
    Signed-off-by: Brett Logan <lindluni@github.com>

--- src/kick.js
+++ src/kick.js
@@ -1,9 +1,9 @@
-const utils = require("../lib/utils");
+const utils = require('../lib/utils');
 
-(async function main() {
-    const org = process.env.ORG
-    const repo = process.env.REPO
-    const appClient = await utils.createGitHubAppClient()
-    const client = await appClient.getInstallationOctokit(process.env.GH_APP_INSTALLATION_ID)
-    await client.inactiveUsers.kick(org, repo)
+(async function main () {
+  const org = process.env.ORG
+  const repo = process.env.REPO
+  const appClient = await utils.createGitHubAppClient()
+  const client = await appClient.getInstallationOctokit(process.env.GH_APP_INSTALLATION_ID)
+  await client.inactiveUsers.kick(org, repo)
 })()
