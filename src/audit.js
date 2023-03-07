d1effbec764774a66e43c8e6905b70c13ebdadea
Brett Logan <lindluni@github.com>
2023-03-07 11:34:32 -0500

    Register secrets and register failures
    Signed-off-by: Brett Logan <lindluni@github.com>

--- src/audit.js
+++ src/audit.js
@@ -1,8 +1,14 @@
+const core = require('@actions/core')
 const utils = require('../lib/utils');
 
-(async function main () {
+const main = async () => {
+  await utils.registerSecrets()
   const appClient = await utils.createGitHubAppClient()
   const client = await appClient.getInstallationOctokit(process.env.GH_APP_INSTALLATION_ID)
   const since = await utils.getSince(7)
   await client.inactiveUsers.audit(process.env.ORG, since)
-})()
+}
+
+main().catch(err => {
+    core.setFailed(err.message)
+})
