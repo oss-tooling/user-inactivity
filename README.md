3ea28b8a7d981be998b459ef97ef50deb9ceacc3
Tyler Garner <garnertb@gmail.com>
2023-03-24 15:53:40 -0400

    Make the inactivity time configurable.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Automation that helps GitHub Enterprise Administrators monitor and remove inacti
 The tool is completely driven by actions which perform three main tasks:
 
 1. `Audit` - Analyze the enterprise audit log for the last activity performed by each user and stores a record of the activity in a json file.
-2. `Report` - Analyzes the json log from step 1 and creates an issue to notify each user with no activity within 90 days that they will be removed from the organization if they do not perform an action.  Users can simply reply to the issue to preserve their account.
+2. `Report` - Analyzes the json log from step 1 and creates an issue to notify each user with no activity within the configured amount of days that they will be removed from the organization if they do not perform an action.  Users can simply reply to the issue to preserve their account.
 3. `Kick` - Removes inactive users from the organization and closes notification issues.
 
 ## Set up
--- src/report.js
+++ src/report.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core')
 const utils = require('../lib/utils')
+const database = require('../lib/database')
 
-const notificationBody = process.env.AUDIT_NOTIFICATION_BODY || `This organization automatically removes inactive users from its GitHub organization.  Your account has been inactive for 90 days and is pending removal.
+const notificationBody = process.env.AUDIT_NOTIFICATION_BODY || `This organization automatically removes inactive users from its GitHub organization.  Your account has been inactive for ${database.INACTIVE_DURATION} days and is pending removal.
 
- You can maintain access by responding to this issue or by making a commit, creating an issue or PR, or by commenting on something every 90 days. 
+ You can maintain access by responding to this issue or by making a commit, creating an issue or PR, or by commenting on something every ${database.INACTIVE_DURATION} days. 
 `;
-
 const main = async () => {
   const org = process.env.ORG
   const repo = process.env.REPO
